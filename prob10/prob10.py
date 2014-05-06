48da0d90aeaa3382804fce0e223a9304e8f369d4
pedrosorio <mebm.pedrosorio@gmail.com>
2014-05-06 01:59:12 -0700

    Remove problem 10

--- prob10/prob10.py
+++
@@ -1,6 +0,0 @@
-import urllib
-k = raw_input()
-f = urllib.urlopen('http://random.contest.tuenti.net/?input=' + k)
-s = f.read()
-print s
-
--- prob10/python10.py
+++
@@ -1,6 +0,0 @@
-import urllib
-
-data = urllib.urlencode([('input[]', '[]')])
-f = urllib.urlopen('http://random.contest.tuenti.net/',data)
-s = f.read()
-print s
